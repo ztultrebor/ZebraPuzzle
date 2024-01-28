5d34415341448792393f0d7ed702744619236d26
Robert Lutz <robertlutzi@gmail.com>
2024-01-27 21:43:51 -0500

    refactor into list comprehension

--- zebra.py
+++ zebra.py
@@ -6,25 +6,20 @@ def solve():
     petses = list(permutations(('dog', 'snails', 'fox', 'horse', 'zebra')))
     drinks = list(permutations(('coffee', 'tea', 'milk', 'orange juice', 'water')))
     smokes = list(permutations(('old gold', 'kools', 'chesterfields', 'lucky strike', 'parliaments')))
-    for w in wheres:
-        if w.index('norwegian')==0:
-            for d in drinks:
-                if (d.index('milk')==2
-                and w.index('ukranian')==d.index('tea')):
-                    for h in houses:
-                        if (h.index('ivory')-h.index('green')==1
-                        and w.index('englishman')==h.index('red')
-                        and abs(w.index('norwegian')-h.index('blue'))==1
-                        and d.index('coffee')==h.index('green')):
-                            for s in smokes:
-                                if (s.index('kools')==h.index('yellow')
-                                and s.index('lucky strike')==d.index('orange juice')
-                                and w.index('japanese')==s.index('parliaments')):
-                                    for p in petses:
-                                        if (w.index('spaniard')==p.index('dog')
-                                        and s.index('old gold')==p.index('snails')
-                                        and abs(s.index('chesterfields')-p.index('fox'))==1
-                                        and abs(s.index('kools')-p.index('horse'))==1):
-                                            return (h,w,p,d,s)
+    return [(h,w,p,d,s)
+            for w in wheres
+                if w.index('norwegian')==0
+            for d in drinks
+                if (d.index('milk')==2 and w.index('ukranian')==d.index('tea'))
+            for h in houses
+                if (h.index('ivory')-h.index('green')==1 and w.index('englishman')==h.index('red')
+                and d.index('coffee')==h.index('green') and abs(w.index('norwegian')-h.index('blue'))==1)
+            for s in smokes
+                if (s.index('kools')==h.index('yellow') and s.index('lucky strike')==d.index('orange juice')
+                and w.index('japanese')==s.index('parliaments'))
+            for p in petses
+                if (w.index('spaniard')==p.index('dog')  and s.index('old gold')==p.index('snails')
+                and abs(s.index('chesterfields')-p.index('fox'))==1 and abs(s.index('kools')-p.index('horse'))==1)][0]
+                                        
 
 print(solve())
