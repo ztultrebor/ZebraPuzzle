79d3e9737b069608415b7b954bc4779b7bc2d1ef
Robert Lutz <robertlutzi@gmail.com>
2024-01-27 21:23:23 -0500

    reorganization of iterations to improve performance

--- zebra.py
+++ zebra.py
@@ -7,26 +7,25 @@ drinks = list(permutations(('coffee', 'tea', 'milk', 'orange juice', 'water')))
 smokes = list(permutations(('old gold', 'kools', 'chesterfields', 'lucky strike', 'parliaments')))
 
 ans = []
-for h in houses:
-    if h.index('ivory')-h.index('green')==1:
-        for w in wheres:
-            if (w.index('englishman')==h.index('red')
-            and w.index('norwegian')==0
-            and abs(w.index('norwegian')-h.index('blue'))==1):
-                for p in petses:
-                    if w.index('spaniard')==p.index('dog'):
-                        for d in drinks:
-                            if (d.index('coffee')==h.index('green')
-                            and w.index('ukranian')==d.index('tea')
-                            and d.index('milk')==2):
-                                for s in smokes:
-                                    if (s.index('old gold')==p.index('snails')
-                                    and s.index('kools')==h.index('yellow')
+for w in wheres:
+    if w.index('norwegian')==0:
+        for d in drinks:
+            if (d.index('milk')==2
+            and w.index('ukranian')==d.index('tea')):
+                for h in houses:
+                    if (h.index('ivory')-h.index('green')==1
+                    and w.index('englishman')==h.index('red')
+                    and abs(w.index('norwegian')-h.index('blue'))==1
+                    and d.index('coffee')==h.index('green')):
+                        for s in smokes:
+                            if (s.index('kools')==h.index('yellow')
+                            and s.index('lucky strike')==d.index('orange juice')
+                            and w.index('japanese')==s.index('parliaments')):
+                                for p in petses:
+                                    if (w.index('spaniard')==p.index('dog')
+                                    and s.index('old gold')==p.index('snails')
                                     and abs(s.index('chesterfields')-p.index('fox'))==1
-                                    and abs(s.index('kools')-p.index('horse'))==1
-                                    and s.index('lucky strike')==d.index('orange juice')
-                                    and w.index('japanese')==s.index('parliaments')):
+                                    and abs(s.index('kools')-p.index('horse'))==1):
                                         ans.append((h,w,p,d,s))
 
 print(ans)
-
